8df86b2342da2f1e524ee0e2cc1816e844f01d2d
Anatoly <chistowick@yandex.ru>
2020-07-29 17:31:42 +0300

    Поправил верстку, убрал лишний отступ от фото на малых экранах. Добавил ещё одну промежуточную фазу адаптива для sm и md брейкпоинтов

--- resources/views/publications_list.blade.php
+++ resources/views/publications_list.blade.php
@@ -37,15 +37,15 @@
 
 @if($publication['type'] == 'Reviews')
 
-<div class="card pr-4 pb-2 mt-3 {{ $publication['type'] }}">
+<div class="card pb-2 mt-3 {{ $publication['type'] }}">
 
     <div class="row justify-content-center">
-        <div class="col-12 col-sm-4 mt-0 mt-sm-4 mt-md-0">
+        <div class="col-12 col-sm-6 col-md-4 mt-0 mt-sm-4 mt-md-0">
             <a href="{{ route('publication', [ 'name' => $publication['name'] ]) }}">
                 <img class="card-img-top rounded img-reviews" src="{{ asset($publication['main_image']) }}">
             </a>
         </div>
-        <div class="col-12 col-sm-8">
+        <div class="col-12 col-sm-6 col-md-8">
 
             <div class="row justify-content-center">
                 <div class="col-11 text-center mt-3">
@@ -56,25 +56,25 @@
             </div>
 
             <div class="row text-right">
-                <div class="col-8 ml-auto mb-3 mt-0">
+                <div class="col-8 ml-auto mb-3 mr-3 mt-0">
                     <h6>({{ $publication['author'] }})</h6>
                 </div>
             </div>
 
             <div class="row no-gutters justify-content-center">
 
-                <div class="col-10 col-sm-6">
-                    <ul class="liked pl-1 pr-1">{!! $publication['liked'] !!}</ul>
+                <div class="col-10 col-sm-12 col-md-6">
+                    <ul class="liked pl-1 pr-3">{!! $publication['liked'] !!}</ul>
                 </div>
 
-                <div class="col-10 col-sm-6">
-                    <ul class="disliked pl-1 pr-1">{!! $publication['disliked'] !!}</ul>
+                <div class="col-10 col-sm-12 col-md-6">
+                    <ul class="disliked pl-1 pr-3">{!! $publication['disliked'] !!}</ul>
                 </div>
 
             </div>
 
             <div class="row text-right">
-                <div class="col-8 ml-auto mt-2">
+                <div class="col-8 ml-auto mt-2 mr-3">
                     <h5 class="text-muted">{{ $publication['datestamp'] }}</h5>
                 </div>
             </div>
