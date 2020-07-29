85b16f109ecee45923052e49fe6ab75921a62fce
Anatoly <chistowick@yandex.ru>
2020-07-29 15:31:24 +0300

    Переделал верстку страницы со списком публикаций под адаптив Bootstrap

--- resources/views/publications_list.blade.php
+++ resources/views/publications_list.blade.php
@@ -1,58 +1,92 @@
-@extends('template.main_template') <!--Расширяем главный шаблон-->
+@extends('template.main_template')
+<!--Расширяем главный шаблон-->
 
 <!--Content of the "publications list" page-->
 
 @section('content')
 <!--content start-->
 
-<?php foreach ($publications_list as $publication) : ?> 
+@foreach($publications_list as $publication)
 
-    <?php if ($publication['type'] == 'Articles') : ?>
+@if($publication['type'] == 'Articles')
 
-        <div class="publications <?= $publication['type'] ?>">
-            <br><hr><br>
-
+<div class="card p-3 p-md-4 mt-4 {{ $publication['type'] }}">
+    <div class="row justify-content-center">
+        <div class="col-10 text-center mt-3">
             <a href="{{ route('publication', [ 'name' => $publication['name'] ]) }}">
-                <h3 class="title"><?= $publication['title'] ?></h3>
+                <h3 class="card-title">{{ $publication['title'] }}</h3>
             </a>
+        </div>
+    </div>
 
-            <p style="margin-top: 16px;"><?= $publication['short_description'] ?></p>
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <p>{!! $publication['short_description'] !!}</p>
+        </div>
+    </div>
 
-            <p style="text-align: right;"><?= $publication['datestamp'] ?></p>
+    <div class="row text-right">
+        <div class="col-8 ml-auto mt-2">
+            <h5 class="text-muted">{{ $publication['datestamp'] }}</h5>
+        </div>
+    </div>
 
-            <hr style="clear: both"><br></div>
+</div>
 
-    <?php endif; ?>
+@endif
 
-    <?php if ($publication['type'] == 'Reviews') : ?>
+@if($publication['type'] == 'Reviews')
 
-        <div class="publications <?= $publication['type'] ?>">
-            <br><hr><br>
+<div class="card pr-4 pb-2 mt-3 {{ $publication['type'] }}">
 
+    <div class="row justify-content-center">
+        <div class="col-12 col-sm-4 mt-0 mt-sm-4 mt-md-0">
             <a href="{{ route('publication', [ 'name' => $publication['name'] ]) }}">
-                <img class="imgReviews" src="{{ asset($publication['main_image']) }}">
+                <img class="card-img-top rounded img-reviews" src="{{ asset($publication['main_image']) }}">
             </a>
+        </div>
+        <div class="col-12 col-sm-8">
 
-            <a href="{{route('publication', [ 'name' => $publication['name'] ])}}">
-                <h3 class="title"><?= $publication['title'] ?></h3>
-            </a>
-
-            <span class="author">(<?= $publication['author'] ?>)</span>
-
-            <div class="liked-disliked">
-                <ul class="liked"><?= $publication['liked'] ?></ul>
-                <ul class="disliked"><?= $publication['disliked'] ?></ul>
+            <div class="row justify-content-center">
+                <div class="col-11 text-center mt-3">
+                    <a href="{{ route('publication', [ 'name' => $publication['name'] ]) }}">
+                        <h3 class="card-title">{{ $publication['title'] }}</h3>
+                    </a>
+                </div>
             </div>
 
-            <br>
-            <p class="datestamp"><?= $publication['datestamp'] ?></p>
+            <div class="row text-right">
+                <div class="col-8 ml-auto mb-3 mt-0">
+                    <h6>({{ $publication['author'] }})</h6>
+                </div>
+            </div>
 
-            <hr style="clear: both"><br>
+            <div class="row no-gutters justify-content-center">
+
+                <div class="col-10 col-sm-6">
+                    <ul class="liked pl-1 pr-1">{!! $publication['liked'] !!}</ul>
+                </div>
+
+                <div class="col-10 col-sm-6">
+                    <ul class="disliked pl-1 pr-1">{!! $publication['disliked'] !!}</ul>
+                </div>
+
+            </div>
+
+            <div class="row text-right">
+                <div class="col-8 ml-auto mt-2">
+                    <h5 class="text-muted">{{ $publication['datestamp'] }}</h5>
+                </div>
+            </div>
+
         </div>
+    </div>
 
-    <?php endif; ?>
+</div>
 
-<?php endforeach; ?>
+@endif
+
+@endforeach
 
 <!--content end-->
 @endsection
